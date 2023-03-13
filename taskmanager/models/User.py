0ed85b5ebfc8ddf072e21ea3156a5c9eb8a8a9ed
KonstantyFlint <43811569+KonstantyFlint@users.noreply.github.com>
2023-03-13 12:15:41 +0100

    fixed fatal user error

--- taskmanager/models/User.py
+++ taskmanager/models/User.py
@@ -1,12 +1,13 @@
-from django.contrib.auth.base_user import BaseUserManager
-
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 import django.db.models as md
-from django.contrib.auth.models import AbstractUser
 
 
 class UserManager(BaseUserManager):
     def create_user(self, **kwargs):
         password = kwargs.pop('password')
+        kwargs["is_superuser"] = False
+        kwargs["is_staff"] = False
         user = self.model(**kwargs)
         user.set_password(password)
         user.save()
@@ -14,19 +15,19 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, **kwargs):
         password = kwargs.pop('password')
+        kwargs["is_superuser"] = True
+        kwargs["is_staff"] = True
         user = self.model(**kwargs)
         user.set_password(password)
-        user.is_superuser = True
-        user.is_staff = True
         user.save()
         return user
 
 
-class User(AbstractUser):
+class User(AbstractBaseUser, PermissionsMixin):
     username = md.CharField(max_length=30, unique=True)
     first_name = md.CharField(max_length=50)
     last_name = md.CharField(max_length=50)
-
+    is_staff = md.BooleanField(default=False)
     USERNAME_FIELD = "username"
 
     objects = UserManager()
